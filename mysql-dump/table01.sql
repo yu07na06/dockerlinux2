3053d9e45626b2e9d7a1828e2204628378b9833f
yu07na06 <dbsk7885@daum.net>
2021-10-07 10:12:37 +0900

    first commit

---
+++ mysql-dump/table01.sql
@@ -0,0 +1,9 @@
+create database db01;
+use db01;
+
+CREATE TABLE `table01` (
+  `fullname` varchar(45) NOT NULL,
+  `age` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`fullname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
